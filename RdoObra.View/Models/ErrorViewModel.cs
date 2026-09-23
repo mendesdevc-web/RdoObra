141dd98ac7444a9aa57887604cec5c21453d2927
mendesdevc-web <mendes.dev.c@gmail.com>
2026-09-23 16:15:33 -0300

    Adicionando camada view

---
+++ RdoObra.View/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RdoObra.View.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
